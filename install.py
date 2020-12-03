7f3916ed99ad30a67bf51ec99f5bb7b48a6183ae
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-03 15:30:28 +0200

    Update install.py

--- install.py
+++ install.py
@@ -4,11 +4,12 @@ cd
 git clone https://github.com/cccvev/ELSHAPEH-ELMODAMER
 mv ELSHAPEH-ELMODAMER $HOME
 mv ELSHAPEH-ELMODAMERRR $HOME
+cd
 cd ELSHAPEH-ELMODAMERRR
 mkdir .vmaker
 cd
 cd ELSHAPEH-ELMODAMER
-mv * /data/data/com.termux/files/home/ELSHAPEH-ELMODAMER√/.vmaker
+mv * /data/data/com.termux/files/home/ELSHAPEH-ELMODAMERRR/.vmaker
 cd
 cd ELSHAPEH-ELMODAMERRR
 python ELSHAPEH-ELMODAMER.py
